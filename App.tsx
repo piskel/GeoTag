32ae2b352585213d912438d5cc35629754c5eab8
Froidevaux Luc <luc.froideva@he-arc.ch>
2021-09-28 14:59:09 +0200

    Fixed map and added geolocation

--- App.tsx
+++ App.tsx
@@ -14,110 +14,77 @@ import {
   Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {  } from 'react-native-maps';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'light';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Geolocation from 'react-native-geolocation-service';
 
-const App = () => {
+
+const App = () =>
+{
+
+  // GEOLOCATION
+  Geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+    },
+    (error) => {
+      // See error code charts below.
+      console.log(error.code, error.message);
+    },
+    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+  );
+
+  ///////////////////////////////////
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      // alignItems: 'center',
+      // justifyContent: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+      flex:1
+    },
+    scrollview: {
+      flex:1
+    }
+  });
+
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <SafeAreaView style={styles.container} >
+      <View style={styles.container}>
+        <MapView style={styles.map}>
+        </MapView>
+      </View>
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        
-        <View>
-          <MapView></MapView>
-          <Button title="test123" onPress={()=>Alert.alert('p')}/>
-        </View>
-        {/* <Header /> */}
-        {/* <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Publnnioenioenc">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View> */}
+        style={styles.scrollview}
+        // style={styles.container}
+        >
+
+          <Button title="OK" onPress={() => Alert.alert('Button with adjusted color pressed')}></Button>
+
       </ScrollView>
     </SafeAreaView>
-  );
-};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  );
+}
+
 
 export default App;
